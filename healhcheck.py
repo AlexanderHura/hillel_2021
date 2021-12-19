9d74125401911f5e493cb48a446abee45faab8c8
alex <alexander_gura@ukr.net>
2021-12-19 20:01:22 +0200

    test commite 2

--- healhcheck.py
+++ healhcheck.py
@@ -1,7 +1,8 @@
 # virtualvenvwraper
 # venv
-# pip venv
+# Pipenv
 
-
-def healthcheck() -> str:
+def healthcheck(arg_1:int) ->str:
     print("Code works fine")
+random_list = [1, 2, 3]
+print('linter flake8')
