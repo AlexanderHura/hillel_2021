3607c9db426bf15f3acc93323f3468d1b2a8d691
alex <alexander_gura@ukr.net>
2021-12-20 18:05:40 +0200

    test_commit_linter_flake8_4

--- healhcheck.py
+++ healhcheck.py
@@ -1,8 +1,11 @@
 
 
+# virtualvenvwraper
+# venv
+# Pipenv
 def healthcheck(arg_1: int) -> str:
     print(f'Code works fine{arg_1}')
 
 
 random_list = [1, 2, 3]
-print('linter flake8')
+print(f'linter flake8')
