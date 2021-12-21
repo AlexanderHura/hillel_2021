0aad25fa7f75bd0a219e609a9133d7897c28bba8
alex <alexander_gura@ukr.net>
2021-12-21 19:43:49 +0200

    test_commit_linter_flake8_7

--- healhcheck.py
+++ healhcheck.py
@@ -1,12 +1,12 @@
-
-
 # virtualvenvwraper
 # venv
 # Pipenv
 """Test metod healthcheck."""
+
+
 def healthcheck(arg_1: int) -> str:
-    """healthcheck"""
-    print(f'Code works fine{arg_1}')
+    """Healthcheck."""
+    print(f"Code works fine{arg_1}")
 
 
 RANDOM = [1, 2, 3]
