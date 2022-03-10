1cf1a9573d47e49428d31ffbe3e13f58e1ede648
alex <alexander_gura@ukr.net>
2022-03-10 10:53:48 +0200

    HW15

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Like, Post
+#@admin.register(Like) 
+class LikeTabularInLine(admin.TabularInline):
+        model = Like
+
+
+#@admin.register(Post) 
+class PostAdmin(admin.ModelAdmin):
+    inlines = [LikeTabularInLine]
+    class Meta:
+        model = Post
+    
+admin.site.register(Post, PostAdmin)
+
+#admin.site.register(Like)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
+import re
+from turtle import back
 from django.db import models
+# from django.contrib.auth.models import User
+#from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+#User = get_user_model()
+class Db(models.Model):
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=False, on_delete=models.SET_NULL
+        )
+   
+
+    created_at = models.DateTimeField(auto_created=True)
+    updated_At = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    
+
+class Post(Db):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    image = models.CharField(max_length=255, null=True, blank=True)
+    pass
+    def __str__(self) -> str:
+        return 'Title: %s |  User: %s | Content: %s' % (self.title, self.user, self.content)
+        #return f"{self.title, self.user}"
+
+    
+
+class Like(Db):   
+    post = models.ForeignKey("Post", null=False, blank=False, on_delete=models.CASCADE)
+    
+    
+
